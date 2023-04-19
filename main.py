ce1e6ac9c770a47af33c933c81f241137b13dbf5
UglystephanHestonovich <127040306+UglystephanHestonovich@users.noreply.github.com>
2023-04-19 09:57:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ def hello_world():
     return c
 
 @app.route('/sum2/', methods=['POST'])
-def hello_world():
+def hello_world2():
     a = list(request.json['data'])
     b = sum(a)
     c = {'sum': b}
     return c
 
 @app.route('/sum3/', methods=['POST'])
-def hello_world():
+def hello_world3():
     a = request.json.values()
     b = sum(a)
     c = {'sum': b}
     return c
 
 @app.route('/nextround/', methods=['POST'])
-def hello_world():
+def hello_world4():
     n = request.json['n']
     k = request.json['k']
     d = request.json['data']
@@ -39,7 +39,7 @@ def hello_world():
     return {"res": a}
 
 @app.route('/lexographically/', methods=['POST'])
-def hello_world():
+def hello_world5():
     a = request.json['a'].lower()
     b = request.json['b'].lower()
     c = 0
@@ -55,6 +55,20 @@ def hello_world():
             c += -1
 
     return {'res': c}
+
+@app.route('/imt/', methods=['POST'])
+def hello_world6():
+    a = request.json
+    if 'imt' not in a:
+        a['imt'] = int(a['weight'] / (a['height'] / 100)**2)
+        return {'status': 'ok', 'imt': a['imt']}
+    if 'weight' not in a:
+        a['weight'] = int(a['imt'] * (a['height'] / 100)**2)
+        return {'status': 'ok', 'weight': a['weight']}
+    if 'height' not in a:
+        a['height'] = int((((a['weight'] / a['imt']) ** 0.5) * 100))
+        return {'status': 'ok', 'height': a['height']}
+
 
 app.run(host='128.1.12.94', port=5000)
 
