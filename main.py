f46f7b02feb7bf7b9fcc56fbdca1e905aacf7a28
UglystephanHestonovich <127040306+UglystephanHestonovich@users.noreply.github.com>
2023-04-19 10:56:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,31 @@ def hello_world6():
     if 'height' not in a:
         a['height'] = int((((a['weight'] / a['imt']) ** 0.5) * 100))
         return {'status': 'ok', 'height': a['height']}
-
+    
+dogyears = {1:14, 1.5:20, 2:24, 3:30, 4:36, 5:40, 6:42, 7:49, 8:56, 9:63, 10:65, 11:71, 12:75}
+humanyears = {14:1, 20:1.5, 24:2, 30:3, 36:4, 40:5, 42:6, 49:7, 56:8, 63:9, 65:10, 71:11, 75:12}
+dogmonths = {2:14, 6:5, 8:9}
+humanmonths = {14:2, 5:6, 9:8}
+@app.route('/lebo1/', methods=['POST'])
+def hello_world7():
+    if request.json['type'] == 'dog' and request.json['units'] == 'years':
+        a = dogyears[request.json['n']]
+        return {'n': a, 'units': 'years', 'type': 'human'}
+    elif request.json['type'] == 'human' and request.json['units'] == 'years' and request.json['n'] >= 14:
+        b = humanyears[request.json['n']]
+        return {'n': b, 'units': 'years', 'type': 'dog'}
+    elif request.json['type'] == 'human' and request.json['units'] == 'month' and request.json['n'] == 14:
+        b = humanmonths[request.json['n']]
+        return {'n': b, 'units': 'month', 'type': 'dog'}
+    elif request.json['type'] == 'dog' and request.json['units'] == 'month' and request.json['n'] > 2:
+        a = dogmonths[request.json['n']]
+        return {'n': a, 'units': 'years', 'type': 'human'}
+    elif request.json['type'] == 'dog' and request.json['units'] == 'month' and request.json['n'] == 2:
+        a = dogmonths[request.json['n']]
+        return {'n': a, 'units': 'month', 'type': 'human'}
+    elif request.json['type'] == 'human' and request.json['units'] == 'years' and request.json['n'] <= 9:
+        b = humanmonths[request.json['n']]
+        return {'n': b, 'units': 'month', 'type': 'dog'}
 
 app.run(host='128.1.12.94', port=5000)
 
